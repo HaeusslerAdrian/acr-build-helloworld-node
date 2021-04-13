431d3316c65615c808dbcbf1667c1fd6200a122d
HaeusslerAdrian <a.haeussler@adresta.ch>
2021-04-13 13:51:32 +0200

    added routes

--- server.js
+++ server.js
@@ -4,8 +4,21 @@ const port = 80
 
 const server = http.createServer((request, response) => {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.write('Hello World\n')
-  response.end('Version: ' + process.env.NODE_VERSION + '\n')
+  
+  var url = req.url;
+      
+  if(url ==='/about') {
+      res.write(' Welcome to about us page'); 
+      res.end(); 
+  }
+  else if(url ==='/contact') {
+      res.write(' Welcome to contact us page'); 
+      res.end(); 
+  }
+  else {
+      res.write('Hello World!'); 
+      res.end(); 
+  }
 })
 
 server.listen(port)
