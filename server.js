efa4a1c31b581b91fa885a709da0f5eba0bd4784
HaeusslerAdrian <a.haeussler@adresta.ch>
2021-04-13 15:33:29 +0200

    fix routes

--- server.js
+++ server.js
@@ -2,10 +2,29 @@ const http = require('http')
 
 const port = 80
 
-const server = http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.write('Hello World\n')
-  response.end('Version: ' + process.env.NODE_VERSION + '\n')
+const server = http.createServer((request, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  /* res.write('Hello World\n');
+  res.end('Version: ' + process.env.NODE_VERSION + '\n'); */
+
+  var url = req.url;
+      
+  if(url ==='/about') {
+      res.write(' Welcome to about us page'); 
+      res.end(); 
+  }
+  else if(url ==='/contact') {
+      res.write(' Welcome to contact us page'); 
+      res.end(); 
+  }
+  else if(url ==='/test') {
+    res.write('I am a sweet test'); 
+    res.end(); 
+}
+  else {
+      res.write('Hello World!'); 
+      res.end(); 
+  }
 })
 
 server.listen(port)
